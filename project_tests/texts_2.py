db577602a5a8b0830a89a8121b044f12c91af057
SpalarniaOdpaduwOrganicznych <247094@edu.p.lodz.pl>
2024-06-07 22:01:39 +0200

    wtf is wrong with this repo

---
+++ project_tests/texts_2.py
@@ -0,0 +1,43 @@
+# texts_2.py
+"""**5G caused the COVID pandemic** 
+There is no scientific evidence to support the claim that 5G technology caused the COVID-19 pandemic. The virus is caused by a specific pathogen, SARS-CoV-2, and not by radio waves or electromagnetic fields. The misconception may have arisen due to a lack of understanding about the technology and the virus, as well as the fact that both 5G and COVID-19 are new and rapidly developing.
+https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public/myth-busters
+https://www.cdc.gov/coronavirus/2019-ncov/faq.html#5G
+
+**The COVID-19 pandemic was planned by the authorities** 
+The COVID-19 pandemic is not a hoax or a planned event by the authorities. It is a real and serious disease caused by a virus. The virus has been identified and studied by scientists and medical professionals around the world, and its existence and impact have been documented in numerous peer-reviewed studies and reports.
+https://www.who.int/emergencies/diseases/novel-coronavirus-2019
+https://www.cdc.gov/coronavirus/2019-ncov/index.html
+
+**COVID-19 is not as contagious as scientists claim** 
+COVID-19 is highly contagious and can spread easily from person to person through respiratory droplets and aerosols. This misconception may have arisen due to a lack of understanding about the virus and its transmission, as well as the fact that some people who are infected may not show symptoms. However, even asymptomatic individuals can spread the virus, and the virus can also survive on surfaces for a period of time.
+https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/how-covid-spreads.html
+https://www.who.int/news-room/q-a-detail/coronavirus-disease-covid-19-how-is-it-transmitted
+
+**COVID-19 has never existed** 
+COVID-19 is a real and serious disease caused by a virus called SARS-CoV-2. The virus has been identified and studied by scientists and medical professionals around the world, and its existence and impact have been documented in numerous peer-reviewed studies and reports.
+https://www.who.int/emergencies/diseases/novel-coronavirus-2019
+https://www.cdc.gov/coronavirus/2019-ncov/index.html
+
+**COVID-19 can be treated with essential oils, homeopathy, etc.** 
+There is no scientific evidence to support the claim that essential oils, homeopathy, or other alternative therapies can treat or prevent COVID-19. The best way to prevent COVID-19 is to get vaccinated, wear masks, practice social distancing, and wash hands frequently. If you do get sick, seek medical attention from a healthcare professional.
+https://www.who.int/news-room/q-a-detail/traditional-complementary-and-integrative-medicine-for-covid-19
+https://www.cdc.gov/coronavirus/2019-ncov/hcp/treatment-guidance.html
+
+
+**Vaccines contain microchips** 
+There is no scientific evidence to support the claim that vaccines contain microchips or any other tracking devices. This misconception may have arisen due to a lack of understanding about the ingredients and manufacturing processes used in vaccines. Vaccines contain various components, such as antigens, adjuvants, and stabilizers, that help to stimulate an immune response and protect against infectious diseases.
+https://www.cdc.gov/vaccines/parents/vaccine-ingredients/index.html
+https://www.who.int/news-room/q-a-detail/vaccines-and-immunization-myths-debunked
+
+
+**Vaccines cause COVID, autism and other diseases** 
+There is no scientific evidence to support the claim that vaccines cause COVID-19, autism, or any other diseases. Vaccines have been thoroughly tested and are safe and effective at preventing infectious diseases. The misconception that vaccines cause autism may have arisen due to a fraudulent study that was published in 1998 and later retracted.
+https://www.cdc.gov/vaccinesafety/concerns/autism.html
+https://www.who.int/news-room/q-a-detail/vaccines-and-immunization-myths-debunked
+
+
+**Masks and other safety precautions are not needed especially after vaccination** 
+Masks and other safety precautions, such as social distancing and hand hygiene, are still important even after vaccination. This is because vaccines are not 100% effective at preventing infection, and some people may not develop a strong immune response to the vaccine. In addition, it is not yet clear whether vaccinated individuals can still transmit the virus to others.
+https://www.cdc.gov/coronavirus/2019-ncov/vaccines/after-vaccination.html
+https://www.who.int/emergencies/diseases/novel-coronavirus-2019/covid-19-vaccines"""
